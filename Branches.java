95cf0db961aadbdbbb5643afbc3bcbedc2e10ace
Simon Weiß <111361107+Batenton@users.noreply.github.com>
2025-11-27 11:30:48 +0000

    Branches.java

--- Branches.java
+++ Branches.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Branches{
+    
+    public static void main(String args[])
+    {
+        Scanner meinScanner = new Scanner(System.in);
+        int Alter = meinScanner.nextInt();
+        int bier = 0;
+        if(Alter >= 16){
+            bier++;
+        System.out.println("Bier ausschenken");;
+        }
+        else{
+            System.out.println("Kein Alkohol unter 16");
+        }
+        System.out.println("Es wurden :"+ bier + " Bier ausgeschenkt.");
+    }
+}
