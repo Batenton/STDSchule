35d323e449d45ed9cf72c1d05251d4a538745d7a
Simon Weiß <111361107+Batenton@users.noreply.github.com>
2025-12-11 12:38:45 +0100

    Urlaubstage

--- Urlaubstage.java
+++ Urlaubstage.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+public class Urlaubstage.java{
+    public static void main (String[] arg){
+        int at;
+        int bhig;
+        int tgt;
+        int uta;
+        System.out.println("Was ist das Alter?")
+        Scanner atScanner = new Scanner (System.in);
+        at = atScanner;
+        System.out.println("Was ist der Behinderungsgrad?")
+        bhig = bhigScanner
+        Scanner bhigScanner;
+        }
+    }
+}
